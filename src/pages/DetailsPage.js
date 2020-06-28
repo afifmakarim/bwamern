aa8b08c010b4d80daf10dc478528fc2dd0ec43a5
afifmakarim <afifmakarim88@gmail.com>
2020-06-28 17:29:47 +0700

    front end done

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
-
+import Fade from 'react-reveal/Fade'
 import Header from "parts/Header"
 import PageDetailTitle from "parts/PageDetailTitle"
+import FeaturedImage from "parts/FeaturedImage"
+import PageDetailDescription from "parts/PageDetailsDescription"
+import BookingForm from "parts/BookingForm"
+import Categories from "parts/Categories"
+import Testimony from 'parts/Testimony'
+import Footer from 'parts/Footer'
 
 import ItemDetails from "json/itemDetails.json"
 
@@ -24,6 +30,24 @@ export default class DetailsPage extends Component {
                 breadcrumb={breadcrumbList}
                 data={ItemDetails}
             ></PageDetailTitle>
+            <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>
+            <section className="container">
+                <div className="row">
+                    <div className="col-7 pr-5">
+                        <Fade bottom>
+                            <PageDetailDescription data={ItemDetails}></PageDetailDescription>
+                        </Fade>
+                    </div>
+                    <div className="col-5">
+                        <Fade bottom>
+                            <BookingForm itemDetails={ItemDetails} ></BookingForm>
+                        </Fade>
+                    </div>
+                </div>
+            </section>
+            <Categories data={ItemDetails.categories}></Categories>
+            <Testimony data={ItemDetails.testimonial}></Testimony>
+            <Footer/>
            </>
         )
     }
